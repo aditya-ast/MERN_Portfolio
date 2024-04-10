a56df991a3cd4b541feb853007c60223b2926515
aditya-ast <adisingh7478@gmail.com>
2024-04-11 02:39:31 +0530

    dekhte hai

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    {/* <section id='Services'><Parallax type='services'/></section>
+    <section id='Services'><Parallax type='services'/></section>
     <section><Services/></section>
     <section id='Portfolio'><Parallax type='portfolio'/></section>
-    <Portfolio/>
+    {/* <section><Portfolio/></section>
     <section id='Contact'><Contact/></section> */}
     {/* <Test/> */}
   </div>;
